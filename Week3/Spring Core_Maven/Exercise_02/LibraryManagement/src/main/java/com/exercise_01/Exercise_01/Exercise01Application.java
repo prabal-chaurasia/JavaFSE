89f9eb9658fcfa21023152f42ef63a43d5d7d1ee
Prabal Chaurasia <164780708+prabal-chaurasia@users.noreply.github.com>
2025-07-05 11:28:07 +0530

    Week03 Hands-on
    Spring Core_Maven
    spring-data-jpa-handson

---
+++ Week3/Spring Core_Maven/Exercise_02/LibraryManagement/src/main/java/com/exercise_01/Exercise_01/Exercise01Application.java
@@ -0,0 +1,13 @@
+package com.exercise_01.Exercise_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise01Application.class, args);
+	}
+
+}
---
+++ Week3/Spring Core_Maven/Exercise_02/LibraryManagement/src/test/java/com/exercise_01/Exercise_01/Exercise01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise_01.Exercise_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
